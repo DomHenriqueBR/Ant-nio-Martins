93354b5898795b94bac44cb33f8edacd0d0894fe
Dom Henrique <joaohenriqueds24@gmail.com>
2024-12-07 17:48:27 -0300

    Código do carrossel

--- embarquenessecarrossel.js
+++ embarquenessecarrossel.js
@@ -1,13 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const carrossel = document.querySelector(".carrossel_fotos");
-    const imagens = carrossel.querySelectorAll("img");
-    let indiceAtual = 0;
-  
-    function alternarImagem() {
-      imagens[indiceAtual].classList.remove("active");
-      indiceAtual = (indiceAtual + 1) % imagens.length;
-      imagens[indiceAtual].classList.add("active");
-    }
-    setInterval(alternarImagem, 3000);
-  });
-  
+let currentIndex = 0;
+const images = document.querySelectorAll('.carrossel img');
+const totalImages = images.length;
+
+function showImage() {
+    images.forEach((img, index) => {
+        img.style.display = 'none';
+    });
+    images[currentIndex].style.display = 'block';
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % totalImages;
+    showImage();
+}
+
+function prevImage() {
+    currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+    showImage();
+}
+
+showImage();
+
+document.querySelector('.next').addEventListener('click', nextImage);
+document.querySelector('.prev').addEventListener('click', prevImage);
